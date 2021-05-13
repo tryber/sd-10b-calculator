1645682d4faf4e4d402ed0be15bc7d1d77a05930
EduardoSantosF <eduardo.debest81@gmail.com>
2021-05-13 18:13:02 -0300

    fix factorial

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function sum(a, b) {
 function factorial(num) {
   if (num === 0 || num === 1)
     return 1;
-  for (var i = num - 1; i >= 1; i--) {
+  for (let i = num - 1; i >= 1; i--) {
     num *= i;
   }
   return num;
