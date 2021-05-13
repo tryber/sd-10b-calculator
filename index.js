e8229aabb76e282be971740283867aa1f5f72729
EduardoSantosF <eduardo.debest81@gmail.com>
2021-05-13 13:39:30 -0300

    adicionando função factorial

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function subtract(a, b) {
 function sum(a, b) {
   return a + b
 }
+
+function factorial(num) {
+  var result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
+}
