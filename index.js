c65d2605b1eecad29d4e8e1079088905d5112c14
Maitê Caetano <maitecaetano@MacBook-Air-de-Maite.local>
2021-05-13 18:21:20 -0300

    resolving conflict

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ function subtract(a, b) {
 function sum(a, b) {
   return a + b;
 }
-<<<<<<< HEAD
 
 function mult(a, b) {
   return a * b;
 }
-=======
->>>>>>> 73dd04c0ad9c94eff678d51690a193284bfcb074
