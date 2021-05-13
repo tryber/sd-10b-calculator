d6b400811c9f62ccd10f86ce1904cbd6918c34b4
EduardoSantosF <eduardo.debest81@gmail.com>
2021-05-13 18:10:05 -0300

    adicionado função fatorial

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function subtract(a, b) {
 function sum(a, b) {
   return a + b;
 }
+
+function factorial(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
